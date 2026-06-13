cc96da0dd49c92bdb2b420141df75bc138276904
Alex La Guidara <alexlaguidara5@gmail.com>
2026-06-13 18:03:29 +1000

    Add basic identifiers

--- components/app/settingsmodal.tsx
+++ components/app/settingsmodal.tsx
@@ -1,4 +1,5 @@
 import { Modal, Button } from "@heroui/react";
+import { useSession } from "@/lib/auth-client";
 
 interface SettingsModalProps {
   isOpen: boolean;
@@ -6,13 +7,19 @@ interface SettingsModalProps {
 }
 
 export default function SettingsModal({ isOpen, onClose }: SettingsModalProps) {
+  const session = useSession();
+  console.log(session);
+
   return (
     <Modal isOpen={isOpen} onOpenChange={onClose}>
       <Modal.Backdrop>
       <Modal.Container placement="center">
         <Modal.Dialog>
           <Modal.Header>Settings - Account</Modal.Header>
-          <Modal.Body>work in progress</Modal.Body>
+          <Modal.Body>
+            <p>Email: {session?.data?.user?.email}</p>
+            <p>Username: {session?.data?.user?.username}</p>
+          </Modal.Body>
           <Modal.Footer>
             <Button variant="outline" onPress={onClose}>
               Close
